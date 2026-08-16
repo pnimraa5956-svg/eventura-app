d50a0c9ed6cc686ca18fa469fd9a4cecce175fb6
pnimraa5956-svg <pnimraa5956@gmail.com>
2026-08-17 03:33:53 +0530

    Update server.js port binding for Render

--- eventura-app/frontend/src/api.js
+++ eventura-app/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://eventura-app.onrender.com/api',
 });
 
 // Automatically attach Authorization token to requests if logged in
